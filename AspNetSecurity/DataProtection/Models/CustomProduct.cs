aa55fa99f4c68efd7c1c081ec99aebfb04a49bfb
Eren Sezen <erenkaansez17@gmail.com>
2025-09-06 18:12:34 +0000

    addDataProtector

---
+++ AspNetSecurity/DataProtection/Models/CustomProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataProtection.Models;
+
+public partial class Product
+{
+    [NotMapped] // veri tabanında böyle bir kolon olmadığını belirtir
+    public string EncrypedId { get; set; }
+
+}
